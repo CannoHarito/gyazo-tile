7910ee46dc7b6a74c6a0f02369fc81ca23e5f48c
CannoHarito <45878603+CannoHarito@users.noreply.github.com>
2026-05-16 18:29:32 +0900

    refactor: extract file handling logic into setFilesToIndexedDB function

--- browser/sw.ts
+++ browser/sw.ts
@@ -7,24 +7,52 @@ globalThis.addEventListener("fetch", (event) => {
   if (event.request.method === "POST" && url.pathname === "/sw/inbox") {
     event.respondWith((async () => {
       const formData = await event.request.formData();
-
       const files = formData.getAll("imageData[]")
         .filter((f): f is File => f && f instanceof File || false);
-      const openRequest = indexedDB.open("inbox");
-      openRequest.onupgradeneeded = () => {
-        const db = openRequest.result;
-        db.createObjectStore("inbox");
-      };
-      openRequest.onsuccess = () => {
-        const db = openRequest.result;
-        const transaction = db.transaction("inbox", "readwrite");
-        transaction.oncomplete = () => db.close();
-        const store = transaction.objectStore("inbox");
-        for (const file of files) {
-          store.put({ file });
-        }
-      };
+      try {
+        await setFilesToIndexedDB(files);
+      } catch (error) {
+        return Response.json({ error }, { status: 400 });
+      }
       return Response.redirect("/", 303);
     })());
   }
 });
+
+const setFilesToIndexedDB = (
+  files: File[],
+  { dbName = "inbox", storeName = "inbox" } = {},
+) => {
+  return new Promise<void>((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+    request.onupgradeneeded = () => {
+      const db = request.result;
+      db.createObjectStore(storeName, { autoIncrement: true });
+    };
+    request.onsuccess = () => {
+      const db = request.result;
+      const transaction = db.transaction(storeName, "readwrite");
+      const store = transaction.objectStore(storeName);
+      for (const file of files) {
+        const data = { name: file.name, file: file };
+        store.add(data);
+      }
+      transaction.oncomplete = () => {
+        db.close();
+        resolve();
+      };
+
+      transaction.onerror = () => {
+        db.close();
+        reject(transaction.error);
+      };
+      transaction.onabort = () => {
+        db.close();
+        reject(new Error("transaction is aborted."));
+      };
+    };
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
